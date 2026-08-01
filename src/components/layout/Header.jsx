d1c0f2d497108cbccc560e2c9b0bf6355a6bb398
maynardvillar <maynardl.villar@gmail.com>
2026-08-01 13:46:49 +0800

    Practical Exam Part 5: Styling and layout complete

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
   const isReady = true
 
   return (
-    <header>
+    <header className="card">
       <h1>{getGreeting()}, welcome to My Study Toolkit</h1>
       <p>Prepared by {firstName + ' ' + lastName}</p>
       <p>You have {totalTopics * 2} flashcards ready to review</p>
--- src/components/layout/ItemList.jsx
+++ src/components/layout/ItemList.jsx
@@ -1,14 +1,15 @@
 import Item from './Item'
 import Divider from './Divider'
+import './ItemList.css'
 
 function ItemList() {
   return (
-    <section>
+    <section className="item-list">
       <Item title="HTML & CSS" description="Structure and style the web" />
       <Divider />
       <Item title="JavaScript" description="Add logic and interactivity" />
       <Divider />
-      <Item title="React" description="Build components and UIs" />
+      <Item title="React" description="Build components and UI" />
     </section>
   )
 }
